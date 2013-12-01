7e8052569aca79410e6135bab158d5eeefc542c3
Patrick Wang <kk1fff@patrickz.net>
2013-12-01 22:24:49 +0800

    handle no predefined image error

--- web.js
+++ web.js
@@ -6,17 +6,24 @@ function initWebApp(port) {
   var app = express(),
       defer = Q.defer();
 
-  // Pre defined images.
+  app.use(express.logger());
+
+  // Predefined images.
   // url: /calendar/predefined_photo/2013/11
-  app.get(/\/calendar\/([A-Za-z0-9]+)\/([0-9]{4})\/([1-9][0-2]{0,1})/,
-          function(req, resp){
+  app.get(/\/calendar\/([A-Za-z0-9]+)\/([0-9]{4})\/([1-9][0-2]{0,1})/, function(req, resp){
     // try to get pre-defined calendar maker.
-    var loader = new calMaker.PredefinedImageLoader(req.params[0]);
-    calMaker.make(loader,
-                  parseInt(req.params[1]), /* year */
-                  parseInt(req.params[2]), /* month */
-                  'jpeg', /* output type */
-                  resp /* output stream */);
+
+    try {
+      var loader = new calMaker.PredefinedImageLoader(req.params[0]);
+      calMaker.make(loader,
+                    parseInt(req.params[1]), /* year */
+                    parseInt(req.params[2]), /* month */
+                    'jpeg', /* output type */
+                    resp /* output stream */);
+    } catch (e) {
+      resp.statusCode = 500;
+      resp.end("Error: " + e);
+    }
   });
 
   app.listen(port);
